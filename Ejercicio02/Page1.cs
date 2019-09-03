b1c4f381b5a6b2cde1de0962cf6868c58792f770
AlvaroSebastian98 <alvaro.sebastian98@gmail.com>
2019-09-02 20:42:42 -0500

    first commit

---
+++ Ejercicio02/Page1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Ejercicio02
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
